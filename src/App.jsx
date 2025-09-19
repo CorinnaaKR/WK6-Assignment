0dd69a0291ac985e0d235f66c106e5250272ac93
Corinna <x.corinna.x@hotmail.co.uk>
2025-09-19 15:02:46 +0100

    created state and effects, updated readMe

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import "./App.css";
 
+
+
+
+import { useState, useEffect } from React;
+import React from "react";
+
+const [imageData, setImageData] = useState([]);
 export default function App() {
+  
+    useEffect(() => {
+    //we cannot make the component function async
+    async function getData() {
+      const response = await fetch("https://week-6-api.vercel.app/api/images");
+      const data = await response.json();
+      const wrangledData = data.Images;
+      setImageData(wrangledData);
+    }
+    getData();
+  }, []);
+  console.log(Images);
+  
   return (
     <>
       <h1>Gallery</h1>
