8c01cb30f23512fd76b4ef85de19fd069b9fd8a9
Corinna <x.corinna.x@hotmail.co.uk>
2025-09-20 22:48:03 +0100

    went back over my images components and passing the porps, updated my readMe

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
 import "./App.css";
-
-
-
-
+import React from "React";
 import { useState, useEffect } from React;
-import React from "react";
+import Images from "./components/Images";
 
+//UseState & UseEffect to identify components to be rendered from API and fetch the data
+
+//UseState
 const [imageData, setImageData] = useState([]);
+
+//Not sure if this bit of code is right, or if it is, if it should be somewhere else?
 export default function App() {
   
+
+  //USE EFFECT
     useEffect(() => {
-    //we cannot make the component function async
     async function getData() {
       const response = await fetch("https://week-6-api.vercel.app/api/images");
       const data = await response.json();
-      const wrangledData = data.Images;
-      setImageData(wrangledData);
+      setImageData(data);
     }
     getData();
   }, []);
+
   console.log(Images);
   
+
+//Map creates a new array of images, and the key helps identify each image.
+
+
   return (
     <>
-      <h1>Gallery</h1>
+      <h1 className="Images">Ribberting Stuff</h1>
+    
+      <div>
+        {images.map((item) => {
+        return (
+          <Images
+            key={item.id}
+            title={item.title} 
+            url={item.url}
+            alt={item.alt}
+            />
+        );
+})};
+      </div>
     </>
   );
+}
+
+function handleClick() {
+  
+}
+  setImageData()
 }
---
+++ src/components/Images.jsx
@@ -0,0 +1,10 @@
+export default function Images(props) {
+    const imageData = {}
+    return (
+     <section className="thumbnails">      
+        <div>{title={props.title} img src={props.url} alt={props.alt}        
+        {/* < onClick={handleClick} /> */}}
+        </div>
+</section>
+    );
+}
